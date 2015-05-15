216064b1a6675326d16ded08053daeadbc79e42a
juan <florez.esteban@gmail.com>
2015-05-15 11:19:57 +0300

    New tree path for modules. Everything is relative to the module root. The module root ends on /

--- app/src/main/java/com/sofia/oppi/animationengine/ModuleGsonParser.java
+++ app/src/main/java/com/sofia/oppi/animationengine/ModuleGsonParser.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.sofia.oppi.Constants;
 import com.sofia.oppi.dbUtils.StringXtractor;
 
 import java.util.ArrayList;
@@ -21,7 +22,7 @@ public class ModuleGsonParser {
 
         String contentString ="";
         try {
-            contentString = StringXtractor.getStringFromFile(pathToFile+"content.json");
+            contentString = StringXtractor.getStringFromFile(pathToFile+ Constants.CONTENT_FILE);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,7 +36,6 @@ public class ModuleGsonParser {
            {
                String chapterJsonString =
                           StringXtractor.getStringFromFile(pathToFile+
-                                  "/chapters/"+
                           array.get(i).getFileName());
                Chapter chapter = gson.fromJson(chapterJsonString,Chapter.class);
 
@@ -47,11 +47,9 @@ public class ModuleGsonParser {
                {
                    String ScenesJsonString =
                            StringXtractor.getStringFromFile(pathToFile +
-                                  "/chapters/"+
                             sceneArray.get(j).getJsonFile()
                            );
                    Log.d(TAG, "parsing "+sceneArray.get(j).getJsonFile() );
-                   Log.d(TAG, ScenesJsonString);
                    ArrayList<Frame> tmpFramesArray = gson.fromJson(ScenesJsonString,
                                                     new TypeToken<ArrayList<Frame>>(){}
                                                         .getType());
@@ -66,10 +64,10 @@ public class ModuleGsonParser {
            }
 
         }
-
-        Log.i(TAG,thePackage.toString());
-        Log.d(TAG,thePackage.getChapter(0).getContentSceneAt(0).getFrames()
-                .get(0).getImages().get(0).getBitmapName() );
+       // Uncommet: If want to see the final product of the parsing.
+       // Log.i(TAG,thePackage.toString());
+       // Log.d(TAG,thePackage.getChapter(0).getContentSceneAt(0).getFrames()
+       //         .get(0).getImages().get(0).getBitmapName() );
         thePackage.setRoot(pathToFile);
         return thePackage;
 
